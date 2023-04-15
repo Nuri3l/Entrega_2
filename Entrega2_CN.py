52c41e006f1b1a6789cb2ef6754c803759bf98f0
Nahuel Cotta <113398214+Nuri3l@users.noreply.github.com>
2023-04-14 23:44:29 -0300

    Update Entrega2_CN.py

--- Entrega2_CN.py
+++ Entrega2_CN.py
@@ -1,4 +1,17 @@
-import random
+""" ENTREGA 2
+
+    10. Dada una lista de nombres de estudiantes y dos listas con sus notas en un curso, escriba un
+        programa que manipule dichas estructuras de datos para poder resolver los siguientes puntos:
+        A. Generar una estructura con todas las notas relacionando el nombre del estudiante con las
+           notas. Utilizar esta estructura para la resolución de los siguientes items.
+        B. Calcular el promedio de notas de cada estudiante.
+        C. Calcular el promedio general del curso.
+        D. Identificar al estudiante con la nota promedio más alta.
+        E. Identificar al estudiante con la nota más baja.
+Nota:
+• Las 3 estructuras están ordenadas de forma que los elementos en la misma posición corresponden
+a un mismo alumno.
+• Realizar funciones con cada item """
 
 nombres = ''' 'Agustin', 'Alan', 'Andrés', 'Ariadna', 'Bautista', 'CAROLINA', 'CESAR', 
 'David', 'Diego', 'Dolores', 'DYLAN', 'ELIANA', 'Emanuel', 'Fabián', 'Facundo', 
@@ -8,6 +21,8 @@ nombres = ''' 'Agustin', 'Alan', 'Andrés', 'Ariadna', 'Bautista', 'CAROLINA', '
 'Nicolás',  'Nancy', 'Noelia', 'Pablo', 'Priscila', 'Sabrina', 'Tomás', 'Ulises',
 'Yanina' '''.replace(",","") .replace("'","")
 
+nombres = nombres.split()
+
 notas_1 = [81,  60, 72, 24, 15, 91, 12, 70, 29, 42, 16, 3, 35, 67, 10, 57, 11, 69, 
            12, 77, 13, 86, 48, 65, 51, 41, 87, 43, 10, 87, 91, 15, 44, 
            85, 73, 37, 42, 95, 18, 7,     74, 60, 9, 65, 93, 63, 74]
@@ -16,10 +31,26 @@ notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37
            64, 13, 8, 87, 14, 14, 49, 27, 55, 69, 77, 59, 57, 40, 96, 24, 30, 73,
            95, 19, 47, 15, 31, 39, 15, 74, 33, 57, 10]
 
+def primer_inciso(nombres): 
+    alumnos_calificados = {alumno:[nota1,nota2] for alumno,nota1,nota2 in zip(nombres,notas_1,notas_2)}
+    return alumnos_calificados
 
-alumnos_calificados = zip(nombres,notas_1,notas_2)
+alumnos_calificados = primer_inciso(nombres)
 
-print((alumnos_calificados))
+def segundo_inciso(alumnos_calificados):
+    promedio_alumnos = {}
+    for alumno in alumnos_calificados.keys():
+        promedio = sum(alumnos_calificados[alumno])/len(alumnos_calificados[alumno])
+        promedio_alumnos[alumno] = promedio
+    return promedio_alumnos
+
+
+def tercer_inciso(alumnos_calificados):
+    promedio_general = sum(alumnos_calificados.values())/len(alumnos_calificados)
+    return promedio_general
+
+
+
 
 
 
